af39a123284197e0ee83a29315d5a290ab2b2470
Maaz <ahmedmaazu43@gmail.com>
2025-12-11 20:03:25 +0500

    first exercise first task completed

---
+++ firstExercise.py
@@ -0,0 +1,5 @@
+student_name = str("Ali Khan")
+student_age = int(16)
+student_grade = str("10th")
+student_gpa = float(3.8)
+print(f"{student_name} is {student_age} years old and currently in {student_grade} grade with a GPA of {student_gpa}.")
