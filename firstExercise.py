3946099b9ae92645f344d72b0e738880f4e37fd0
Maaz <ahmedmaazu43@gmail.com>
2025-12-11 23:13:44 +0500

    exercise 2

--- firstExercise.py
+++ firstExercise.py
@@ -5,3 +5,16 @@ student_gpa = float(3.8)
 print(f"{student_name} is {student_age} years old and currently in {student_grade} grade with a GPA of {student_gpa}.")
 
 # Output: Ali Khan is 16 years old and currently in 10th grade with a GPA of 3.8.
+
+
+coffee_price = float (2.5)
+sandwich_price = float (4.0)
+cake_price = float (3.0)
+customer_name = str ("Ayesha")
+
+print("Customer Name:", customer_name, "Coffee: $", coffee_price, "Sandwich: $", sandwich_price, "Cake: $", cake_price)
+print("Total Bill: $", coffee_price + sandwich_price + cake_price)
+
+#Output:
+#Customer Name: Ayesha Coffee: $ 2.5 Sandwich: $ 4.0 Cake: $ 3.0
+#Total Bill: $ 9.5
