f2085d87f7f974c3cfc0934b88faa551c99bc5c0
Maaz <ahmedmaazu43@gmail.com>
2025-12-11 20:05:32 +0500

    first exercise first task completed

--- firstExercise.py
+++ firstExercise.py
@@ -3,3 +3,5 @@ student_age = int(16)
 student_grade = str("10th")
 student_gpa = float(3.8)
 print(f"{student_name} is {student_age} years old and currently in {student_grade} grade with a GPA of {student_gpa}.")
+
+# Output: Ali Khan is 16 years old and currently in 10th grade with a GPA of 3.8.
